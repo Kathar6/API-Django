229a646db4ac668dcbbf290a87a18745b17f25f0
mate458 <matepabon@gmail.com>
2020-05-18 22:43:35 -0500

    Version 1.0 tutorial_django

---
+++ api_django/computers/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+#from django.template import loader
+from .models import Question, Choice
+# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'computers/index.html'
+    context_object_name = 'latest_question_list'
+    def get_queryset(self):
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'computers/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'computers/results.html'
+
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     #template = loader.get_template('computers/index.html')
+#     context = {'latest_question_list': latest_question_list}
+#     return render(request, 'computers/index.html', context)
+
+# We used Http404 for print a custom 404 error, in this case we use the get_object_404 shorcut method,
+# that combine the Http404 exception and the Model.objects.get() method
+
+
+# def detail(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'computers/detail.html', {'question': question})
+
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'computers/results.html', {'question': question})
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'computers/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice"
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('computers:results', args=(question.id,)))
